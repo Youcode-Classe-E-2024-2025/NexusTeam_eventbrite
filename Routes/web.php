261c6a645bfe8bd77672e23913a198f8bd9ef082
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-10 18:38:33 +0100

    add routes of logIn and signUP in web.php

--- Routes/web.php
+++ Routes/web.php
@@ -4,5 +4,6 @@ use App\Core\Router;
 $router = new Router();
 
 $router->add("GET", "/", "UserController@index");
-
+$router->add("GET", "/sign-up", "signUpController@signUp");
+$router->add("GET", "/login", "LoginController@login");
 $router->dispatch();
