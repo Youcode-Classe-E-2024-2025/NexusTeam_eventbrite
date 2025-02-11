cd5fc48358d4d459f2377c73046554f492c79867
achraf <achrafsikal@gmail.com>
2025-02-11 12:26:45 +0100

    adding validater class and editing organizer dash

--- Routes/web.php
+++ Routes/web.php
@@ -4,6 +4,8 @@ use App\Core\Router;
 $router = new Router();
 
 $router->add("GET", "/", "HomeController@index");
-$router->add("GET", "/oranizer", "OrganizerController@index");
+$router->add("GET", "/organizer", "OrganizerController@index");
+$router->add("GET", "/organizer/sales", "OrganizerController@sales");
+$router->add("GET", "/organizer/events", "OrganizerController@events");
 
 $router->dispatch();
