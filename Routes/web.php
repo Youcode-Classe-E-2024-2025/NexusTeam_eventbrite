d44d0b688a028da5a88b19b33399a8a6828ecd9c
sabri-amine <sabriamine368@gmail.com>
2025-02-11 14:17:53 +0000

    inital commit

--- Routes/web.php
+++ Routes/web.php
@@ -5,13 +5,5 @@ $router = new Router();
 
 $router->add("GET", "/", "UserController@index");
 
+// $router->add('GET',  "/tecktes",'TicketController@index');
 $router->dispatch();
-<?php
-
-$router->get('/', 'TicketController@index');
-$router->get('/ticket/{id}', 'TicketController@show');
-$router->post('/ticket/buy', 'TicketController@buy');
-$router->post('/payment/checkout', 'PaymentController@checkout');
-$router->get('/payment/success', 'PaymentController@success');
-$router->get('/payment/cancel', 'PaymentController@cancel');
-$router->get('/ticket/download/{id}', 'TicketController@downloadPDF');
