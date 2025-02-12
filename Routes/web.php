0503ea5b7b2d3779ffb6b4156a5210a2d636f56d
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-12 09:43:47 +0100

    add facebookSignIn routes in web.php

--- Routes/web.php
+++ Routes/web.php
@@ -9,6 +9,6 @@ $router->add("POST", "/sign-up", "SignUpController@register");
 $router->add("GET", "/login", "LoginController@login");
 $router->add("POST", "/login", "LoginController@authenticate");
 $router->add("GET", "/google-signin", "GoogleSignInController@handlegoogle");
-// $router->add("GET", "/facebook-signin", "FacebookSignInController@handle");
+$router->add("GET", "/facebook-signin", "FacebookSignInController@handle");
 
 $router->dispatch();
