98d717a069cf43a32c2ce2e036a93764176bed6b
tahajaiti <kyojin.rar@gmail.com>
2025-02-12 09:50:09 +0100

    FEAT: added tag creation in bulk successfully

--- App/Controllers/TagController.php
+++ App/Controllers/TagController.php
@@ -2,6 +2,7 @@
 namespace App\Controllers;
 
 use App\Core\Request;
+use App\Core\Session;
 use App\Core\Views;
 use App\Models\Tag;
 
@@ -25,5 +26,30 @@ class TagController{
         }
 
         Views::render('Tag/show', ['category'=> $tag]);
+    }
+
+    public function store(Request $request): void {
+        $tags = $request->get('tags');
+        $tags = array_filter(explode(',', trim($tags)));
+
+        $errors = 0;
+
+        foreach ($tags as $tag){
+            $tag = new Tag(0, $tag);
+            if (!$tag->save()){
+                $errors++;
+            }
+        }
+
+        if ($errors !== 0){
+            Session::set('message', "$errors tags not saved, try them again");
+        } else {
+            Session::set('message', "All tags saved successfully");
+        }
+
+
+        Views::redirect('/tags');
+
+
     }
 }
