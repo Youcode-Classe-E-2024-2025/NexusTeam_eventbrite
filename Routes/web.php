b576e1b591c233309ab20ab1f1ebe4461e304281
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-12 09:43:21 +0100

    add googleSignIn routes in web.php

--- Routes/web.php
+++ Routes/web.php
@@ -8,5 +8,7 @@ $router->add("GET", "/sign-up", "SignUpController@signUp");
 $router->add("POST", "/sign-up", "SignUpController@register");
 $router->add("GET", "/login", "LoginController@login");
 $router->add("POST", "/login", "LoginController@authenticate");
+$router->add("GET", "/google-signin", "GoogleSignInController@handlegoogle");
+// $router->add("GET", "/facebook-signin", "FacebookSignInController@handle");
 
 $router->dispatch();
