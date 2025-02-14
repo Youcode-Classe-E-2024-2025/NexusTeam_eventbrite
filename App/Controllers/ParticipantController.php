f14094a0119799b8ed8343cf924450913a05de7b
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-15 00:28:39 +0100

    add session user in index method into participantController

--- App/Controllers/ParticipantController.php
+++ App/Controllers/ParticipantController.php
@@ -10,8 +10,20 @@ use App\Models\User;
 use App\Core\Database;
 use Exception;
 
+
+
 class ParticipantController {
     public function index(){
-        Views::render('participant');
+          // Vérifie si l'utilisateur est connecté
+    session_start();
+    if (isset($_SESSION['user'])) {
+        // Passer les données utilisateur à la vue
+        $user = $_SESSION['user'];
+        Views::render('participant', ['user' => $user]);
+    } else {
+        // Rediriger l'utilisateur vers la page de connexion si non connecté
+        header("Location: /login");
+        exit;
+    }
     }
 }
